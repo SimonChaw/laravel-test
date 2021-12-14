6198bf0b45a69100990e068e8deac880fbc4e923
Simon Chawla <chawblah@gmail.com>
2021-12-14 16:22:05 -0400

    Extra validation rules

--- app/Http/Requests/MailRequest.php
+++ app/Http/Requests/MailRequest.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class MailRequest extends FormRequest
 {
+    protected $stopOnFirstFailure = true;
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -14,6 +18,10 @@ class MailRequest extends FormRequest
     public function authorize()
     {
         return true;
+    }
+
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
 
     /**
@@ -29,8 +37,20 @@ class MailRequest extends FormRequest
             'emails.*.subject' => 'required|string|min:1',
             'emails.*.body' => 'required|string|min:1|max:384000',
             'emails.*.attachments' => 'sometimes|array',
-            'emails.*.attachments.*.filename' => 'required|string|min:1',
+            'emails.*.attachments.*.filename' => 'required|string|min:1|regex:/^[\w,\s-]+\.[A-Za-z]{3}$/',
             'emails.*.attachments.*.file_data' => 'required|string|min:1',
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array|void
+     */
+    public function messages() {
+        return [
+            'emails.*.to.email' => "':input' is not a valid email",
+            'emails.*.attachments.*.filename.regex' => "':input' is not a valid filename.",
         ];
     }
 }
