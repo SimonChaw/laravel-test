5203e60b4bfbe35b1dd243e41361231b05e7d122
Simon Chawla <chawblah@gmail.com>
2021-12-14 14:30:48 -0400

    Use api tokens in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
+use App\Models\ApiToken;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Str;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected static string $token;
+
+    public function setUp(): void {
+        parent::setUp();
+
+        $token = Str::random(60);
+
+        // Revoke old tokens
+        ApiToken::whereNotNull('api_token')->delete();
+
+        ApiToken::create(['api_token' => hash('sha256', $token)]);
+
+        self::$token = $token;
+    }
 }
